2b947fd1b3d0129046942060feaadd1db2cd8c96
Thomas Debarochez <thomas.barochez+github@gmail.com>
2011-06-25 18:13:23 +0200

    initial commit

---
+++ observer.js
@@ -0,0 +1,74 @@
+#!/usr/bin/env node
+
+var fs = require('fs')
+  , exec = require('child_process').exec
+  , spawn = require('child_process').spawn
+  , child = null
+  , args = []
+  , files = []
+  , double_dashes = false
+  , i = 0;
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', function (chunk) {
+  watch(chunk.split('\n'));
+});
+
+process.argv.forEach(function (arg) {
+  if (++i < 3) {
+    return;
+  }
+  if (arg == '--') {
+    double_dashes = true;
+    return;
+  }
+  if (double_dashes) {
+    files.push(arg);
+  }
+  else {
+    args.push(arg);
+  }
+});
+
+if (args.length < 1
+   || !double_dashes
+   || process.argv.length < 4) {
+  process.stderr.write('Usage: observer [node-options] script.js -- files\n');
+  process.exit(1); 
+}
+
+watch(files);
+setTimeout(start, 100);
+
+function reload() {
+  stop();
+  start();
+}
+
+function stop() {
+  if (child === null) {
+    return;
+  }
+  child.kill();
+}
+
+function start() {
+  var opts = {customFds: [process.stdin, process.stdout, process.stderr],
+              cwd: process.cwd()};
+  child = spawn('node', args, opts);
+}
+
+function watch(files) {
+  files.forEach(function (filename) {
+    if (filename.length <= 3) {
+      return;
+    }
+    fs.watchFile(filename, function (curr, prev) {
+      if (+curr.mtime !== +prev.mtime) {
+        reload();
+      }
+    });
+  }); 
+}
